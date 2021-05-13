fd880cae2c29c427f208b780b2e6d578bb2a10ac
Andrew Schwenn <aschwenn@verizon.net>
2021-05-13 15:42:08 -0500

    better styling of radio

--- src/components/defaults/Radio.tsx
+++ src/components/defaults/Radio.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Input } from '../../types'
 
+const Radio = styled.div`
+  display: flex;
+`
+const Label = styled.label`
+  margin-left: 0.25rem;
+`
+
 export default (props: Input): React.ReactElement => (
-  <>
+  <Radio>
     <input type="radio" {...props} />
-    <label htmlFor={props.id}>{props.id}</label>
-  </>
+    <Label htmlFor={props.id}>{props.id}</Label>
+  </Radio>
 )
